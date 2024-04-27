61979229814c43195510ceec8605052a67a33450
Denwer1 <denwer0204@gmail.com>
2024-04-27 03:00:28 +0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/img/logo.svg";
+import Mouse from "../assets/img/header/mouse.svg";
 
 const Header = () => {
   const navList = [
@@ -20,7 +21,7 @@ const Header = () => {
             </Link>
             <div className="header__nav">
               <nav className="nav">
-                <ul>
+                <ul className="nav__list">
                   {navList.map((obj, i) => {
                     return (
                       <li>
@@ -36,11 +37,27 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="header__title">
-        <div className="container">TITLE</div>
+      <div className="container">
+        <h1 className="header__title">
+          Жилой комплекс <br></br> в историческом центре
+          <Link to="#benefits">
+            <img
+              src={Mouse}
+              alt="Scroll next"
+              className="header__title-icon"
+            ></img>
+          </Link>
+        </h1>
       </div>
       <div className="header__footer">
-        <div className="container">ADDRESS</div>
+        <div className="container">
+          <Link to="#!" className="info">
+            Наб. реки Фонтанки 10-15
+          </Link>
+          <Link to="#!" className="info">
+            8 (812) 123-45-67
+          </Link>
+        </div>
       </div>
     </div>
   );
